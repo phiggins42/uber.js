2e94b2c8122de657ae9e0da25b9ce14569e8770e
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-11 12:13:33 -0400

    Implemented a more robust trim solution.

--- src/string.js
+++ src/string.js
@@ -1,22 +1,25 @@
 (function(uber, has){
 
-    uber.trim = (has("string-trim") && !has("bug-regexp-whitespace")) ?
+    uber.trim = (has("string-trim") && !has("bug-es5-trim")) ?
         function(str){
             return str.trim();
         } :
         (function(){
             var whitespace ="\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002"+
-                "\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029";
-            var beginRE, endRE;
-            if(has("bug-regexp-whitespace")){
-                beginRE = new RegExp("^(\s|[" + whitespace + "])(\s|[" + whitespace + "])*");
-                endRE = new RegExp("(\s|[" + whitespace + "])(\s|[" + whitespace + "])*$");
+                "\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF";
+            var wsPlus, ws;
+            if(has("bug-es5-trim")){
+                wsPlus = new RegExp("^[" + whitespace + "]+");
+                ws = new RegExp("[" + whitespace + "]");
             }else{
-                beginRE = /^\s\s*/;
-                endRE = /\s\s*$/;
+                wsPlus = /^\s+/;
+                ws = /\s/;
             }
             return function(str){
-                return str.replace(beginRE, '').replace(endRE, '');
+                var str = str.replace(wsPlus, ''),
+                    i = str.length;
+                while(ws.test(str.charAt(--i)));
+                return str.substring(0, i + 1);
             }
         })();
 })(uber, has);
