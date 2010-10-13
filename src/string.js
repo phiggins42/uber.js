0d3f6ab1b6a341affce64521baa276129aac5daa
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-13 18:43:42 -0400

    Clean up, logic fix, and make WS regexps more efficient.

--- src/string.js
+++ src/string.js
@@ -1,29 +1,34 @@
 (function(uber, has){
 
-    var trim = uber.trim = (function(){
-        if(has("string-trim") && !has("bug-es5-trim")){
-            trim = function trim(str){
-                return str.trim();
-            };
-        }else{
-            var whitespace ="\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002"+
-                "\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF";
-            var wsPlus, ws;
-            if(has("bug-es5-trim")){
-                wsPlus = new RegExp("^[" + whitespace + "]+");
-                ws = new RegExp("[" + whitespace + "]");
-            }else{
-                wsPlus = /^\s+/;
-                ws = /\s/;
+    var trim;
+
+    if(has("string-trim") && !has("bug-es5-trim")){
+        trim = function trim(str){
+            return str.trim();
+        };
+    }else{
+        var wsPlus = /^\s+/, ws = /\s/;
+        if(has("bug-es5-regexp")){
+            var whitespace = ("\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002"+
+                "\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF").split('');
+            var re = /^\s$/, i = whitespace.length, chr;
+            while(i--){
+                if(re.test(whitespace[i])){
+                    whitespace.splice(i, 1);
+                }
             }
-            trim = function trim(str){
-                var str = str.replace(wsPlus, ''),
-                    i = str.length;
-                while(ws.test(str.charAt(--i)));
-                return str.substring(0, i + 1);
-            };
+            whitespace = "[\\s" + whitespace.join('') + "]";
+            wsPlus = new RegExp("^" + whitespace + "+");
+            ws = new RegExp(whitespace);
         }
-        return trim;
-    })();
+        trim = function trim(str){
+            var str = str.replace(wsPlus, ''),
+                i = str.length;
+            while(ws.test(str.charAt(--i)));
+            return str.substring(0, i + 1);
+        };
+    }
+
+    uber.trim = trim;
 
 })(uber, has);
