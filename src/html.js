0cb16984664ca854ddc29f25ad69ffd646a03f0a
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-13 10:25:14 -0400

    Changed to use named function expressions and prevent leaks. Removed any string -> DOMNode magic.

--- src/html.js
+++ src/html.js
@@ -3,148 +3,198 @@
     var STR = "string";
 
     var byId = uber.byId = (function(){
-        function byIdNameCase(id, doc){
-            var _d = doc || document, te = _d.getElementById(id);
-            // attributes.id.value is better than just id in case the 
-            // user has a name=id inside a form
-            if(te && (te.attributes.id.value == id || te.id == id)){
-                return te;
-            }else{
-                var eles = _d.all[id];
-                if(!eles || eles.nodeName){
-                    eles = [eles];
-                }
-                // if more than 1, choose first with the correct id
-                var i = 0;
-                while((te = eles[i++])){
-                    if((te.attributes && te.attributes.id && te.attributes.id.value == id)
-                    || te.id == id){
-                        return te;
+        if(has("bug-getelementbyid-ids-names")||has("bug-getelementbyid-ignores-case")){
+            byId = function byId(id, doc){
+                var _d = doc || document, te = _d.getElementById(id);
+                // attributes.id.value is better than just id in case the 
+                // user has a name=id inside a form
+                if(te && (te.attributes.id.value == id || te.id == id)){
+                    return te;
+                }else{
+                    var eles = _d.all[id];
+                    if(!eles || eles.nodeName){
+                        eles = [eles];
+                    }
+                    // if more than 1, choose first with the correct id
+                    var i = 0;
+                    while((te = eles[i++])){
+                        if((te.attributes && te.attributes.id && te.attributes.id.value == id)
+                        || te.id == id){
+                            return te;
+                        }
                     }
                 }
-            }
-        }
-        function byIdSpec(id, doc){
-            return (doc||document).getElementById(id);
-        }
-
-        function wrapper(func){
-            return function(id, doc){
-                if(typeof id != STR){
-                    return id;
-                }
-                return func(id, doc);
+            };
+        }else{
+            byId = function byId(id, doc){
+                return (doc||document).getElementById(id);
             };
         }
-
-        if(has("bug-getelementbyid-ids-names")||has("bug-getelementbyid-ignores-case")){
-            return wrapper(byIdNameCase);
-        }
-
-        return wrapper(byIdSpec);
+        return byId;
     })();
 
-    function idWrapper(func){
-        return function(id){
-            if(typeof id == STR){
-                return id = byId(id);
+    var isDescendant = uber.getDescendant = function isDescendant(node, ancestor){
+        try{
+            while(node){
+                if(node === ancestor){
+                    return true;
+                }
+                node = node.parentNode;
             }
-            return func.apply(this, uber.toArray(arguments, 1, [id]));
+        }catch(e){};
+        return false;
+    };
+
+    uber.boxModel = has("css-content-box") ? "content-box" : "border-box";
+    uber.styleProperty = has("css-style-float") ? "styleFloat" : "cssFloat";
+
+    var gcs = uber.getComputedStyle = (function(){
+        var getComputedStyle;
+        if(has("dom-current-style")){
+            gcs = function getComputedStyle(node){
+                // IE (as of 7) doesn't expose Element like sane browsers
+                return node.nodeType == 1 /* ELEMENT_NODE*/ ? node.currentStyle : {};
+            };
+        }else if(has("dom-computed-style")){
+            if(has("bug-computed-style-hidden-zero-height")){
+                gcs = function getComputedStyle(node){
+                    var s;
+                    if(node.nodeType == 1){
+                        var dv = node.ownerDocument.defaultView;
+                        s = dv.getComputedStyle(node, null);
+                        if(!s && node.style){
+                            var d = node.style.display;
+                            node.style.display = "";
+                            s = dv.getComputedStyle(node, null);
+                            node.style.display = d;
+                        }
+                    }
+                    return s || {};
+                };
+            }else{
+                gcs = function getComputedStyle(node){
+                    return node.nodeType == 1 ?
+                        node.ownerDocument.defaultView.getComputedStyle(node, null) : {};
+                };
+            }
         }
-    }
+        getComputedStyle = null;
+        return gcs;
+    })();
 
-    var testElem = document.createElement("DiV"),
-        prefixes = ['Webkit', 'Moz', 'O', 'ms', 'Khtml'],
-        dashRE = /-([a-z])/ig,
-        startRE = /^(.)/,
-        bump = function(all, letter){
-            return letter.toUpperCase();
-        };
+    var getStyleProperty = uber.getStyleProperty = (function(){
+        var testElem = document.createElement("DiV"),
+            prefixes = ['Webkit', 'Moz', 'O', 'ms', 'Khtml'],
+            dashRE = /-([a-z])/ig,
+            startRE = /^(.)/,
+            bump = function(all, letter){
+                return letter.toUpperCase();
+            };
 
-    function getStyleProperty(styleName){
-        var styleName = styleName.replace(dashRE, bump),
-            style = testElem.style,
-            length = prefixes.length;
+        function getStyleProperty(styleName){
+            var styleName = styleName.replace(dashRE, bump),
+                style = testElem.style,
+                length = prefixes.length;
 
-        // test unprefixed
-        if(typeof style[styleName] == STR){
-            return styleName;
-        }
-
-        styleName = styleName.replace(startRE, bump);
-
-        while(--length){
-            if(typeof style[prefixes[length] + styleName] == "string"){
+            // test unprefixed
+            if(typeof style[styleName] == STR){
                 return styleName;
             }
-        }
-        return null;
-    }
-    uber.getStyleProperty = getStyleProperty;
 
-    uber.setSelectable = (function(){
-        function setSelectableCSS(node, selectable){
-            node.style[selectableProperty] = selectable ? trueValue : "none";
-        }
-        function setSelectableDOM(node, selectable){
-            var v = (node.unselectable = selectable ? "" : "on");
-            // TODO: figure this out
-            //d.query("*", node).forEach("item.unselectable = '"+v+"'");
-        }
+            styleName = styleName.replace(startRE, bump);
 
-        var selectableProperty, trueValue = "";
+            while(--length){
+                if(typeof style[prefixes[length] + styleName] == "string"){
+                    return styleName;
+                }
+            }
+            return null;
+        };
+
+        return getStyleProperty;
+    })();
+
+    var setSelectable = uber.setSelectable = (function(){
         if(has("css-selectable")){
-            selectableProperty = getStyleProperty("userSelect");
+            var selectableProperty = getStyleProperty("userSelect"),
+                trueValue = "";
             if(/^(Khtml|Webkit)/.test(selectableProperty)){
                 trueValue = "auto";
             }
-            return setSelectableCSS;
+            setSelectable = function setSelectable(node, selectable){
+                node.style[selectableProperty] = selectable ? trueValue : "none";
+            };
         }else if(has("dom-selectable")){
-            return setSelectableDOM;
+            setSelectable = function setSelectable(node, selectable){
+                var v = (node.unselectable = selectable ? "" : "on");
+                // TODO: figure this out
+                //d.query("*", node).forEach("item.unselectable = '"+v+"'");
+            };
         }else{
-            return function(){
+            setSelectable = function setSelectable(){
                 throw new Error();
             }
         }
+        return setSelectable;
     })();
 
-    uber.setOpacity = (function(){
-        var opacityProp;
-        function setOpacity(node, value){
-            return node.style[opacityProp] = value;
-        }
-        function setOpacityFilter(node, value){
-            var ov = opacity * 100, opaque = opacity == 1;
-            node.style.zoom = opaque ? "" : 1;
-
-            if(!af(node)){
-                if(opaque){
-                    return opacity;
-                }
-                node.style.filter += " progid:" + astr + "(Opacity=" + ov + ")";
-            }else{
-                af(node, 1).Opacity = ov;
+    var setOpacity, getOpacity;
+    (function(){
+        var astr = "DXImageTransform.Microsoft.Alpha";
+        function af(n, f){
+            try{
+                return n.filters.item(astr);
+            }catch(e){
+                return f ? {} : null;
             }
-
-            // on IE7 Alpha(Filter opacity=100) makes text look fuzzy so disable it altogether (Dojo bug #2661),
-            //but still update the opacity value so we can get a correct reading if it is read later.
-            af(node, 1).Enabled = !opaque;
-
-            // TODO: figure out how to do this
-            /*if(node.nodeName.toLowerCase() == "tr"){
-                d.query("> td", node).forEach(function(i){
-                    setOpacityFilter(i, opacity);
-                });
-            }*/
-            return opacity;
-        }
+        };
         if(has("css-opacity")){
-            opacityProp = getStyleProperty("opacity");
-            return idWrapper(setOpacity);
+            var opacityProp = getStyleProperty("opacity");
+            setOpacity = function setOpacity(node, value){
+                return node.style[opacityProp] = value;
+            };
+            getOpacity = function getOpacity(node){
+                return gcs(node).opacity;
+            };
         }else if(has("css-opacity-filter")){
-            return idWrapper(setOpacity);
+            setOpacity = function setOpacity(node, value){
+                var ov = opacity * 100, opaque = opacity == 1;
+                node.style.zoom = opaque ? "" : 1;
+
+                if(!af(node)){
+                    if(opaque){
+                        return opacity;
+                    }
+                    node.style.filter += " progid:" + astr + "(Opacity=" + ov + ")";
+                }else{
+                    af(node, 1).Opacity = ov;
+                }
+
+                // on IE7 Alpha(Filter opacity=100) makes text look fuzzy so disable it altogether (Dojo bug #2661),
+                //but still update the opacity value so we can get a correct reading if it is read later.
+                af(node, 1).Enabled = !opaque;
+
+                // TODO: figure out how to do this
+                /*if(node.nodeName.toLowerCase() == "tr"){
+                    d.query("> td", node).forEach(function(i){
+                        setOpacityFilter(i, opacity);
+                    });
+                }*/
+                return opacity;
+            };
+            getOpacity = function getOpacity(node){
+                try{
+                    return af(node).Opacity / 100;
+                }catch(e){
+                    return 1;
+                }
+            };
         }else{
-            return function(){ throw new Error(); };
+            setOpacity = function setOpacity(){ throw new Error(); };
+            getOpacity = function getOpacity(){ throw new Error(); };
         }
     })();
+    uber.setOpacity = setOpacity;
+    uber.getOpacity = getOpacity;
+
 })(uber, has, document);
