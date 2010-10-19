0b9c01c5724bac567467b4dbe9292d9a07a35420
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-19 10:08:41 -0700

    Moved ist and friends to tests/test.js and added more unit tests.

---
+++ tests/test.js
@@ -0,0 +1,43 @@
+(function(global, document, uber){
+    var toString = {}.toString,
+        func = "[object Function]",
+        arr = "[object Array]"
+    ;
+
+    function ist(a, b){
+        var ta = typeof a,
+            tb = typeof b;
+        if(ta != tb){
+            return false;
+        }
+        var sa = toString.call(a),
+            sb = toString.call(b);
+        if(sa != sb){
+            return false;
+        }
+        if(sa == arr){
+            if(a.length != b.length){
+                return false;
+            }
+            for(var i=a.length; i--;){
+                if(!ist(a[i], b[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return a === b;
+    }
+
+    function outputResult(name, result){
+        var out = uber.byId("output"),
+            li = document.createElement("li");
+
+        li.innerHTML = name + ": " + result;
+        out.appendChild(li);
+        li = null;
+    }
+
+    global['ist'] = ist;
+    global['outputResult'] = outputResult;
+})(this, document, uber);
