cee1e819a925059f3d312213a362125707265946
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-22 13:30:55 -0500

    Changed to remove inner closure.

--- src/xhr.js
+++ src/xhr.js
@@ -1,31 +1,32 @@
 (function(uber, has){
 
-    var getXHR = uber.getXHR = (function(){
-        if(has("activex-enabled") !== null){
+    var getXHR;
+
+    if(has("activex-enabled") !== null){
+        try{
+            new ActiveXObject("Msxml2.XMLHTTP");
+            getXHR = function getXHR(){
+                return new ActiveXObject("Msxml2.XMLHTTP");
+            };
+        }catch(e){
             try{
-                new ActiveXObject("Msxml2.XMLHTTP");
-                getXHR = function getXHR(){
-                    return new ActiveXObject("Msxml2.XMLHTTP");
-                };
+                new ActiveXObject("Microsoft.XMLHTTP");
             }catch(e){
-                try{
-                    new ActiveXObject("Microsoft.XMLHTTP");
-                }catch(e){
-                    getXHR = function getXHR(){
-                        return new ActiveXObject("Microsoft.XMLHTTP");
-                    };
-                }
+                getXHR = function getXHR(){
+                    return new ActiveXObject("Microsoft.XMLHTTP");
+                };
             }
-        }else if(has("native-xhr")){
-            getXHR = function getXHR(){
-                return new XMLHttpRequest();
-            };
-        }else{
-            getXHR = function getXHR(){
-                throw new Error("XMLHttpRequest not available");
-            };
         }
-        return getXHR;
-    })();
+    }else if(has("native-xhr")){
+        getXHR = function getXHR(){
+            return new XMLHttpRequest();
+        };
+    }else{
+        getXHR = function getXHR(){
+            throw new Error("XMLHttpRequest not available");
+        };
+    }
+
+    uber.getXHR = getXHR;
 
 })(uber, has);
