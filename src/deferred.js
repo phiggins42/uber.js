8b99990b267d56568d57b2054983a872177c0ea4
Bryan Forbes <bryan@reigndropsfall.net>
2010-12-07 13:06:55 -0600

    Added uber.Deferreds to src/deferred.js.

--- src/deferred.js
+++ src/deferred.js
@@ -124,6 +124,39 @@
     Deferred.prototype = new uber.Promise;
     Deferred.prototype.constructor = Deferred;
 
+    function Deferreds(defs, canceller){
+        uber.Deferred.call(this, canceller);
+
+        var self = this,
+            finished = 0,
+            resultList = [];
+
+        if(!defs.length){
+            this.resolve([0, []]);
+        }
+        function addResult(succeeded, result, idx){
+            resultList[idx] = [succeeded, result];
+            finished++;
+            if(finished === defs.length){
+                self.resolve(resultList);
+            }
+        }
+        uber.forEach(defs, function(def, i){
+            def.then(
+                function(result){
+                    addResult(true, result, i);
+                },
+                function(error){
+                    addResult(false, error, i);
+                    throw error;
+                }
+            );
+        });
+    }
+    Deferreds.prototype = new Deferred;
+    Deferreds.prototype.constructor = Deferreds;
+
     uber.Deferred = Deferred;
+    uber.Deferreds = Deferreds;
 
 })(uber, has);
